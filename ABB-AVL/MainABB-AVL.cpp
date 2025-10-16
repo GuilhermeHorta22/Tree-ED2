e1a41b842ce2b8664c77df82de8f8db8fd722d7e
GuilhermeHorta22 <ghorta800@gmail.com>
2025-10-16 20:51:49 -0300

    adicionando pos ordem interativo e quantidade de nó

--- ABB-AVL/MainABB-AVL.cpp
+++ ABB-AVL/MainABB-AVL.cpp
@@ -47,7 +47,19 @@ void inserir(Tree **raiz, int info)
 }
 
 //inserir um valor na arvore ABB de maneira RECURSIVA
-
+void inserirRec(Tree **raiz, int info)
+{
+	if(*raiz == NULL) //criterio de parada da recursividade
+		*raiz = criaNo(info);
+	else
+	{
+		if(info > (*raiz)->info)
+			inserirRec(&(*raiz)->dir, info);
+		else
+		if(info < (*raiz)->info)
+			inserirRec(&(*raiz)->esq, info);
+	}
+}
 
 //quantidade de nó na arvore RECURSIVO - pós ordem
 void quantidadeNo(Tree *raiz, int *qtde)
@@ -61,7 +73,28 @@ void quantidadeNo(Tree *raiz, int *qtde)
 }
 
 //quantidade de nó na arvore INTERATIVO
-
+void quantidadeNoInt(Tree *raiz, int *qtde) //utiliza o pre ordem interativo
+{
+	Pilha *p;
+	init(&p);
+	push(&p, raiz);
+	
+	while(!isEmpty(p))
+	{
+		pop(&p, &raiz);
+		
+		while(raiz != NULL)
+		{
+			(*qtde)++;
+			push(&p, raiz);
+			raiz = raiz->esq;
+		}
+		pop(&p, &raiz);
+		raiz = raiz->dir;
+		if(raiz != NULL)
+			push(&p, raiz);
+	}
+}
 
 //buscar um nó da arvore de maneira INTERATIVA
 
@@ -85,3 +118,62 @@ void quantidadeNo(Tree *raiz, int *qtde)
 
 
 //pos ordem INTERATIVO
+void posOrdemInt(Tree *raiz)
+{
+	Pilha *p1, *p2;
+	init(&p1);
+	init(&p2);
+	
+	push(&p1, raiz);
+	push(&p2, raiz);
+	while(!isEmpty(p1))
+	{
+		if(raiz != NULL)
+		{
+			pop(&p1, &raiz);
+			while(raiz != NULL);
+			{
+				push(&p1, raiz);
+				push(&p2, raiz);
+				raiz = raiz->dir; //anda primeiro para direita para ser um pos ordem
+			}
+		}
+		pop(&p1, &raiz);
+		raiz = raiz->esq;
+		if(raiz != NULL)
+			push(&p1, raiz);
+	}
+	
+	//apenas retira da pilha e exibe os valores na ordem certa
+	while(!isEmpty(p2))
+	{
+		pop(&p2, &raiz);
+		printf("\n%d", raiz->info);
+	}
+}
+
+//outra maneira de fazer o pos ordem interativo
+void posOrdemInt2(Tree *raiz)
+{
+	Pilha *p1, *p2;
+	init(&p1);
+	init(&p2);
+	push(&p1, raiz);
+	
+	while(!isEmpty(p1))
+	{
+		pop(&p1, &raiz);
+		push(&p2, raiz);
+		
+		if(raiz->esq != NULL)
+			push(&p1, raiz->esq);
+		if(raiz->dir != NULL)
+			push(&p1, raiz->dir);
+	}
+	
+	while(!isEmpty(p2))
+	{
+		pop(&p2, &raiz);
+		printf("\n%d",raiz->info);
+	}
+}
